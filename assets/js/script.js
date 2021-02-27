681d759532fab640651a7f0dfac76ee0832ec122
Sofiane <sa.dev672@gmail.com>
2021-02-27 01:47:36 +0100

    get the geolocation of a city entered to display the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,36 @@
 class App{
     map;
-
     constructor(){
-        this.getPosition();
+        this.displayMap();
     }
 
-    getPosition(){
-        console.log(navigator)
+    //Affiche la carte centré sur la géolocalisation de l'utilisateur se la géolocalisation est activé, sinon il saisie le nom d'une ville pour centrer la carte
+    displayMap(){
         if (navigator.geolocation){
             navigator.geolocation.getCurrentPosition(position => {
                 const {latitude, longitude} = position.coords;
-                this.loadMap([latitude, longitude])
-            }, () => alert("Veuillez activer la géolocalisati"))
+                this.loadMap([latitude, longitude]);
+            }, () => {
+                const city = prompt("ville");
+                this.getPosition(city)
+            })
         }
     }
 
+    // Affiche la carte centré sur la ville passé en paramètre
+    getPosition(city){
+        fetch(`https://datanova.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&q=${city}`)
+        .then(response => response.json())
+        .then(data => {
+            const [datas] = data.records;
+            const [long, lat] = datas.geometry.coordinates
+            this.loadMap([lat, long]);
+        });
+    }
+
+    //Charge la carte
     loadMap(position){
         this.map = L.map('map').setView(position, 15);
-
-        console.log(L)
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
