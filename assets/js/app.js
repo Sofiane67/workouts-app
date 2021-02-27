e0df8f0a8fe77927479828a6299983afacef56a5
Sofiane <sa.dev672@gmail.com>
2021-02-27 13:33:23 +0100

    adding javascript module architecture

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
-const form = document.querySelector(".form");
+import dom from './dom.js';
 
-class App{
+export default class App {
     map;
-    constructor(){
+    constructor() {
         this.displayMap();
     }
 
     //Affiche la carte centré sur la géolocalisation de l'utilisateur se la géolocalisation est activé, sinon il saisie le nom d'une ville pour centrer la carte
-    displayMap(){
-        if (navigator.geolocation){
+    displayMap() {
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
-                const {latitude, longitude} = position.coords;
+                const { latitude, longitude } = position.coords;
                 this.loadMap([latitude, longitude]);
             }, () => {
                 const city = prompt("ville");
@@ -20,18 +20,18 @@ class App{
     }
 
     // Affiche la carte centré sur la ville passé en paramètre
-    getPosition(city){
+    getPosition(city) {
         fetch(`https://datanova.laposte.fr/api/records/1.0/search/?dataset=laposte_hexasmal&q=${city}`)
-        .then(response => response.json())
-        .then(data => {
-            const [datas] = data.records;
-            const [long, lat] = datas.geometry.coordinates
-            this.loadMap([lat, long]);
-        });
+            .then(response => response.json())
+            .then(data => {
+                const [datas] = data.records;
+                const [long, lat] = datas.geometry.coordinates
+                this.loadMap([lat, long]);
+            });
     }
 
     //Charge la carte
-    loadMap(position){
+    loadMap(position) {
         this.map = L.map('map').setView(position, 15);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -44,10 +44,9 @@ class App{
 
     //Afficher le formulaire
 
-    showForm(){
-        form.classList.remove("form--hidden")
+    showForm() {
+        dom.form.classList.remove("form--hidden")
     }
 
 }
 
-const app = new App();
---
+++ assets/js/dom.js
@@ -0,0 +1,6 @@
+ const domElement = {
+    form: document.querySelector(".form")
+};
+
+export default domElement
+ 
